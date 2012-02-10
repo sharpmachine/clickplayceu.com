9ab40acc8173aba8caf741bd04e34cd77c30c6dd
Jesse Kade <jessekade@gmail.com>
2012-02-10 10:35:06 -0800

    finished individual state page

--- wp-content/themes/videos/single-state_requirements.php
+++ wp-content/themes/videos/single-state_requirements.php
@@ -18,7 +18,59 @@
 					<h2><?php the_title(); ?></h2>
 					
 					<div class="entry">
-						<?php the_content('Continue Reading'); ?>
+						<?php the_content(); ?>
+						
+						<p>Select your profession to quickly jump to that section.</p>
+						
+						<ul class="profession-list">
+							<li><a href="#registered_nurse">Registered Nurse</a></li>
+							<li><a href="#residential_facility_for_the_elderly">Residential Facility for the Elderly</a></li>
+							<li><a href="#certified_nursing_assistant">Certified Nursing Assistant</a></li>
+							<li><a href="#nursing_home_administrator">Nursing Home Administrator</a></li>
+							<li><a href="#supported_living_services">Supported Living Services</a></li>
+						</ul>
+						
+						
+						<ul class="profession-list">
+							<li><a href="#assisted_living_administrator">Assisted Living Administrator</a></li>
+							<li><a href="#licensed_vocational_nurses">Licensed Vocational Nurses</a></li>
+							<li><a href="#psychiatric_technicians">Psychiatric Technicians</a></li>
+							<li><a href="#group_home">Group Home</a></li>
+							<li><a href="#adult_residential_facility">Adult Residential Facility</a></li>
+						</ul>
+						<div class="clear"></div>
+						
+						<h3 id="registered_nurse">Registered Nurse</h3>
+						<?php the_field('registered_nurse'); ?>
+						
+						<h3 id="residential_facility_for_the_elderly">Residential Facility for the Elderly</h3>
+						<?php the_field('residential_facility_for_the_elderly'); ?>
+						
+						<h3 id="certified_nursing_assistant">Certified Nursing Assistant</h3>
+						<?php the_field('certified_nursing_assistant'); ?>
+						
+						<h3 id="nursing_home_administrator">Nursing Home Administrator</h3>
+						<?php the_field('nursing_home_administrator'); ?>
+						
+						<h3 id="supported_living_services">Supported Living Services</h3>
+						<?php the_field('supported_living_services'); ?>
+						
+						<h3 id="assisted_living_administrator">Assisted Living Administrator</h3>
+						<?php the_field('assisted_living_administrator'); ?>
+						
+						<h3 id="licensed_vocational_nurses">Licensed Vocational Nurses</h3>
+						<?php the_field('licensed_vocational_nurses'); ?>
+						
+						<h3 id="psychiatric_technicians">Psychiatric Technicians</h3>
+						<?php the_field('psychiatric_technicians'); ?>
+						
+						<h3 id="group_home">Group Home</h3>
+						<?php the_field('group_home'); ?>
+						
+						<h3 id="adult_residential_facility">Adult Residential Facility</h3>
+						<?php the_field('adult_residential_facility'); ?>
+						
+						
 						
 						<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 					</div>
@@ -32,6 +84,8 @@
 				</div>
 				
 			<?php endif; ?> 
+			
+			
 					</div>
 					
 				</div> <!-- channels-inside -->
